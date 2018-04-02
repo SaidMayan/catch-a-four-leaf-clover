a6c36c4278a5f9465cddbabb5a9fc38dbf030a1a
SaidMayan <Kasemayan@gmail.com>
2018-04-02 16:18:22 -0400

    game: nearly/maybe finished

--- Project-1-Game-Page-Said/src/script1.js
+++ Project-1-Game-Page-Said/src/script1.js
@@ -1,10 +1,9 @@
 $(document).ready(function(){
-
-
+  let playerName = prompt("Enter your name and then we'll get started...");
   let cloversArr = [];
   let containerForClover = $('<div>').addClass('container')
   $('body').append(containerForClover)
-  let numOfClovers = 100;
+  let numOfClovers = 100; // making clovers and adding them to an empty array: 'cloversArr'
   for(let i = 0; i < numOfClovers; i++){
     let divForClovers = $('<div>').addClass('divForClovers');
     let clover = $('<img>').attr('src', 'src/images/clover.png');
@@ -16,7 +15,7 @@ $(document).ready(function(){
 
 
 
-let x = 0;
+let x = 0; // making scorecard
 $('.divForClovers').on('click', function(event){
 $(this).remove();
 x++;
@@ -26,7 +25,7 @@ $('.scorecard').text(`score: ${x}`)
 
 })
 
-setInterval(function(){
+setInterval(function(){ // moving clovers at random
   for(let i = 0; i <= cloversArr.length; i++){
     $(cloversArr[i]).css("top", Math.random() * window.innerHeight);
     $(cloversArr[i]).css("left", Math.random() * window.innerWidth);
@@ -34,40 +33,42 @@ setInterval(function(){
   }
 }, 1000);
 
-let time = setInterval(timer,1000);
+let time = setInterval(timer,1000); //creating a timer for the game
 
-let allotedTime = 10;
+let allotedTime = 30;
 
 function timer() {
   console.log(allotedTime);
   if(allotedTime >= 10){
     $('h2').html(`00:${allotedTime}`);
     allotedTime--;
-    console.log('timer is above 10');
   } else if(allotedTime < 10 && allotedTime > 0){
     $('h2').html(`00:0${allotedTime}`);
     allotedTime--;
-    console.log('timer is below 10');
   } else if(allotedTime === 0){
     $('h2').html(`00:0${allotedTime}`);
-    // console.log(allotedTime);
-    // clearInterval(time);
     alert(`Your final score is: ${x}`);
     clearInterval(time);
     $('.container').children("div").remove();
     $('.rain').remove();
+    $('h2').remove();
+    finalPage();
+  }
+    console.log('functions is over');
+}
+
+ function finalPage(){ // creating final page to load up form with artificial high_scores
+    $('.scorecard').remove();
     $('body').css('background-image', 'url(src/images/Notre-Dame-du-Haut-Chapel-Ronchamp-Le-Corbusier.jpg)');
     let divForForm = $('<div>').addClass('divForForm');
     let highscoresForm = $('<form>').addClass('hsForm');
     let headerForForm = $(' <h1>Highest_Scores</h1>');
     let pForm = $('<p>');
-    $(pForm).html(`E. Badu: 1,000,000<br>Tyrone (from Howard U.): 429,000<br>Ludwig Mies van der Rohe: 20,000<br>Kyrie Irving: 10,000<br>Aaron: 5,000<br>Jon: 1,000<br>Einstein: ${x}<br>`)
+    $(pForm).html(`E. Badu: 1,000,000<br>Tyrone (from Howard U.): 429,000<br>Ludwig Mies van der Rohe: 20,000<br>kyrie irving: 10,000<br>Aaron: 5,000<br>jon: 1,000<br>${playerName}: ${x}<br>`)
     highscoresForm.append(headerForForm);
     highscoresForm.append(pForm);
     divForForm.append(highscoresForm);
     $('body').append(divForForm);
-  }
-    console.log('functions is over');
 }
 
 }); // closing tag for document ready...
